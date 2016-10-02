064251d4fc0be72c42887a8407c8f835d93270b9
duaazahi92 <duaa-zahi@hotmail.com>
2016-10-02 20:43:36 +0400

    assignment 1 solution solved

---
+++ module1-solution/app.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = [ '$scope'];
+  function LunchCheckController($scope) {
+
+    $scope.lunchCheckerBtn = function () {
+        var lunch = $scope.lunchMenu,
+            items, i, count = 0;
+        if(lunch != null && lunch !== "") {
+          items = lunch.split(',')
+          for (i in items ) {
+            var item = items[i].trim();
+              if(item !== "") {
+                 count++;
+              }
+          }
+       }
+        if ( count > 3 ) {
+          $scope.msg = "Too much!";
+        } else if ( count <= 3 && count > 0){
+          $scope.msg = "Enjoy!";
+        } else if ( count <= 0) {
+          $scope.msg = "";
+        }
+    };
+  }
+})();
