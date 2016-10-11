65c78d17d5dd452d5bd98dedd09f248202350b0d
duaazahi92 <duaa-zahi@hotmail.com>
2016-10-11 12:41:42 +0400

    fix

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -38,22 +38,6 @@ function ShoppingListCheckOffService() {
      {
        name: "Chips",
        quantity: "5 bags"
-     },
-     {
-       name: "Bread",
-       quantity: "10 loafs"
-     },
-     {
-       name: "Banana",
-       quantity: "15 pieces"
-     },
-     {
-       name: "Strawberry"
-       quantity: "100 pieces"
-     },
-     {
-       name: "Mango"
-       quantity: "2 big pieces"
      }
    ];
 
