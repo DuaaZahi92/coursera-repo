8566a4d304f267d9f7daded0599fe4f66ce83530
duaazahi92 <duaa-zahi@hotmail.com>
2016-10-11 12:18:58 +0400

    module 2 assignment

---
+++ module2-solution/app.js
@@ -0,0 +1,67 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var tobuy = this;
+  tobuy.items = ShoppingListCheckOffService.getToBuyItems();
+  tobuy.moveTobought = function (itemIndex) {
+    ShoppingListCheckOffService.moveTobought(itemIndex);
+  };
+  tobuy.toBuyItemsCount = ShoppingListCheckOffService.getToBuyItemsLength();
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+  bought.items = ShoppingListCheckOffService.getBoughtItems(); 
+  bought.boughtItemsCount = ShoppingListCheckOffService.getBoughtItemsLength();
+}
+
+function ShoppingListCheckOffService() {
+   var service = this;
+   var tobuy = [
+     {
+       name: "Cookies",
+       quantity: "10"
+     },
+     {
+       name: "Milk",
+       quantity: "2 bottles"
+     },
+     {
+       name: "Chips",
+       quantity: "5 bags"
+     }
+   ];
+
+   var bought = [];
+
+   service.moveTobought = function (itemIndex) {
+     var itemRemoved = tobuy.splice(itemIndex, 1);
+     if(itemRemoved[0] !== 'undefined') {
+       bought.push(itemRemoved[0]);
+     }
+   };
+
+   service.getToBuyItems = function () {
+     return tobuy;
+   }
+   service.getBoughtItems = function () {
+     return bought;
+   }
+   service.getToBuyItemsLength = function () {
+     return tobuy.length;
+   }
+   service.getBoughtItemsLength = function () {
+     return bought.length;
+   }
+}
+
+})();
