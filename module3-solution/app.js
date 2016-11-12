3731733542cde64c01e48b7d68bec159a99145fd
duaazahi92 <duaa-zahi@hotmail.com>
2016-11-13 00:19:27 +0400

    https fix

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -6,7 +6,7 @@ angular.module('NarrowItDown', [])
 .service('MenuSearchService', MenuSearchService)
 .controller('foundItemsController', FoundItemsController)
 .directive('foundItems', FoundItems)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 
 function FoundItems () {
